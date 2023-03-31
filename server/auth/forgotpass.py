4e561f8fefce7cab15f1756de7a4961f55a03c7f
joe1-code <john4m2@gmail.com>
2023-03-31 14:38:54 +0300

    convertion of code into tokens for security

--- server/auth/forgotpass.py
+++ server/auth/forgotpass.py
@@ -1,9 +1,17 @@
 import os
 import random
+import jwt
+from datetime import datetime,timedelta
 from flask import jsonify
 import json
+from flask_session import Session
+from server.models import Users
+from flask_sqlalchemy import model
+from flask_jwt_extended import create_access_token,create_refresh_token
 
-def resetPassword(request, Users):
+
+
+def resetPassword(request, Users, db):
 
  data=json.loads(request.data)
 
@@ -18,4 +26,18 @@ def resetPassword(request, Users):
    
   #Generate code
   code=random.randint(1000,9999)
-  return jsonify({'message':code})
+  print(code)
+
+  #tokenlize the code
+  codetoken=jwt.encode({'id':exist_user.userid, 'exp':datetime.utcnow() + timedelta(seconds=int(os.environ.get('DURATION1'), base=0)), 'place':exist_user.place}, os.environ.get('SECRET_KEY'))
+  
+  print('code',codetoken)
+
+  #post the code to db
+  newcode=Users(code=codetoken)
+  db.session.add(newcode)
+  db.session.commit()
+  return {'message':'posted data'}
+
+  return jsonify({'message':'code sent to db'})
+
