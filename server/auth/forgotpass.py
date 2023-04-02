bd304c9e7bffcce9a4cda0fd316dd93d182521bd
joe1-code <john4m2@gmail.com>
2023-04-02 23:57:59 +0300

    working on exporting phoneNo as data

--- server/auth/forgotpass.py
+++ server/auth/forgotpass.py
@@ -12,21 +12,21 @@ from flask_jwt_extended import create_access_token,create_refresh_token
 
 
 def forgotPassword(request, Users, db):
-
  data=json.loads(request.data)
-
  if data:
   mobile=data['phoneNo']
-  print(mobile)
 
+  
   #check the user if exists
   exist_user=Users.query.filter_by(phoneNo=mobile).first()
   if not exist_user:
    return jsonify({'message':'not a user'})
-   
+
+
   #Generate code
   code=random.randint(1000,9999)
   
+
   #tokenlize the code
   codetoken=jwt.encode({'id':exist_user.userid, 'exp':datetime.utcnow() + timedelta(seconds=int(os.environ.get('DURATION1'), base=0)), 'place':exist_user.place}, os.environ.get('SECRET_KEY'))
   
@@ -35,6 +35,11 @@ def forgotPassword(request, Users, db):
   exist_user.code=codetoken
   db.session.add(exist_user)
   db.session.commit()
+  #import the module function that you will export the mobile data
+  from server.auth.resetpass import export_data
+ 
+  # Pass the form data to the export_data function
+  export_data(mobile)
 
   return jsonify({'message':code})
 
--- server/auth/resetpass.py
+++ server/auth/resetpass.py
@@ -6,17 +6,25 @@ import os
 import jwt
 
 
+def export_data(mobile):
+ phone=(f'mobile:{mobile}')
+ return jsonify({'mobile':phone})
+# phone={}
+# def export_data(mobile):
+#  global phone
+#  phone={'phone':mobile}
+#  return phone
+
 def resetPassword(request, Users):
- 
  data=json.loads(request.data)
  if data:
   #data from UI
-  mobile=data['phoneNo']
+  # mobile=data['phoneNo']
   usercode=data['code']
   newpassword=['password']
-
+  # print(mobile)
   #fetching encryted token from db
-  token = Users.query.with_entities(Users.code).filter_by(phoneNo=mobile).first()
+  token = Users.query.with_entities(Users.code).filter_by(phoneNo=mobilenumber).first()
   print("code:",token)
   #decrypt the token for comparison with usercode
   # newtoken=bytes(token, 'utf-8')
@@ -26,4 +34,4 @@ def resetPassword(request, Users):
   # except Exception as e:
   #  print(e)
   #  return ({'message':'failed to decrypt'})
-  return {"code":token.stringfy()}
+  return {}
