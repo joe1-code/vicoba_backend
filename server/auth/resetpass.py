e6c23cd15a56410cde419b875b0f19ab116913dc
joe1-code <john4m2@gmail.com>
2023-04-03 16:06:55 +0300

    finished decryption of token from db

--- server/auth/resetpass.py
+++ server/auth/resetpass.py
@@ -5,33 +5,42 @@ from flask_jwt_extended import create_access_token,create_refresh_token
 import os
 import jwt
 
-
 def export_data(mobile):
  phone=(f'mobile:{mobile}')
- return jsonify({'mobile':phone})
-# phone={}
-# def export_data(mobile):
-#  global phone
-#  phone={'phone':mobile}
-#  return phone
+ print(phone)
+
 
 def resetPassword(request, Users):
  data=json.loads(request.data)
  if data:
   #data from UI
-  # mobile=data['phoneNo']
+  mobile=data['phoneNo']
   usercode=data['code']
   newpassword=['password']
-  # print(mobile)
   #fetching encryted token from db
-  token = Users.query.with_entities(Users.code).filter_by(phoneNo=mobilenumber).first()
-  print("code:",token)
-  #decrypt the token for comparison with usercode
+  # token = Users.query.with_entities(Users.code).filter_by(phoneNo=mobilenumber).first()
+  token=Users.query.filter_by(phoneNo=mobile).first()
+  newToken=(f"{token.code}")
+
+  if token:
+   newToken=(f"{token.code}")
+   # print (newToken)
+   # print(usercode)
+   #decrypt the token for comparison with usercode
+   print("test")
+   try:
+    # decoded = jwt.decode(newToken, 'SECRET_KEY', algorithms=['HS256'])
+    payload = jwt.decode(newToken, os.environ.get('SECRET_KEY'), algorithms=['HS256'])
+    print(payload)
+    return ({'decrypted':payload})
+   except Exception as e:
+    print(e)
+    return ({'message':'failed to decrypt'})
+
+   return (newToken)
+
+  
+  print(newToken)
   # newtoken=bytes(token, 'utf-8')
-  # try:
-  #  # decoded = jwt.decode(newtoken, 'secret_key', algorithms=['HS256'])
-  #  return ({'decrypted':decoded})
-  # except Exception as e:
-  #  print(e)
-  #  return ({'message':'failed to decrypt'})
+  
   return {}
